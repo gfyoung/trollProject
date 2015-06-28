3f72f7b7752e840a01a871140e0d106b8c0cce13
gfyoung <gfyoung@mit.edu>
2015-06-28 01:14:02 -0400

    Download Test

--- trollSite/trollApp/views.py
+++ trollSite/trollApp/views.py
@@ -1,3 +1,15 @@
+from django.core.servers.basehttp import FileWrapper
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'trollApp/download.html')
+
+def downloadTest(request):
+    filename = 'trollApp/downloads/test.txt'
+    wrapper = FileWrapper(open(filename))
+    content_type = 'text/plain'
+    response = HttpResponse(wrapper, content_type=content_type)
+    response['Content-Disposition'] = 'attachment; filename="sucess.txt"'
+    return response
+    
