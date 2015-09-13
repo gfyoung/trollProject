a2e8760e4cc5049af56652205ae2c082e81ddae5
gfyoung <gyoung@Gregorys-MBP.att.net>
2015-09-13 12:48:59 -0700

    Minor HTML reformat and turned off DEBUG

--- trollSite/trollSite/settings.py
+++ trollSite/trollSite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'h0^+z*mn_+5a6mvpt=_fle30$=wwzbu90_%&ns(u%gcykad-vt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
